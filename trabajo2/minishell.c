03ed6751230057b877e8ec6a92bc2da040cfa5b5
Jesús Toro <jesus.tdd03@gmail.com>
2025-03-24 18:54:25 +0000

    Implemented command ayuda.

--- trabajo2/minishell.c
+++ trabajo2/minishell.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "sala.c"
 
+void print_help();
+
 int main(int argc, char * argv[]) {
 	if(argc != 3) {
 		fprintf(stderr, "Número incorrecto de parámetros.\nEsperado: 2\nRecibido: %i\n", argc-1);
@@ -15,15 +17,26 @@ int main(int argc, char * argv[]) {
 		char command[20];
 		scanf("%s", &command);
 		// TODO: Implementar todos los comandos.
-		if (!strcmp(command, "salir")) {
+		if (!strcmp(command, "cerrar_sala")) {
 			break;
 		} else if (!strcmp(command, "ayuda")) {
-			printf("Lista de comandos:\n");
-			printf("(por implementar)\n"); // TODO: Mostrar lista de comandos
+			print_help();
 		} else {
 			fprintf(stderr, "Comando desconocido.\nEscribe \"ayuda\" para ver la lista de comandos.\n");
 		}
 	}
 	elimina_sala();
 	exit(0);
+}
+
+void print_help() {
+	printf("Lista de comandos:\n");
+	printf("reserva X - Realiza una reserva para la persona con id X.\n");
+	printf("libera X - Libera el asiento de la persona X.\n");
+	printf("estado_asiento X - Muestra el estado del asiento X.\n");
+	printf("estado_sala - Muestra el estado de la sala.\n");
+	printf("cerrar_sala - Cierra la sala.\n");
+	printf("ayuda - Muestra la lista de comandos.\n");
+	
+	// TODO: Terminar lista de comandos
 }
