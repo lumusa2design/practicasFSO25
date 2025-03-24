23bd31f38b245c478d35a873574aca6ed8c7c65d
Jesús Toro <jesus.tdd03@gmail.com>
2025-03-24 17:34:37 +0000

    Refactored nombre to ciudad

--- trabajo2/main.c
+++ trabajo2/main.c
@@ -4,13 +4,9 @@
 
 int main(int argc, char argv[]) {
 	while (true) {
-		char nombre[10];
 		char ciudad[20];
 		int capacidad;
-		
-		printf("Nombre de la sala:");
-		scanf ("%S", &nombre);
-		
+
 		printf("Ciudad:");
 		scanf ("%S", &ciudad);
 		printf("Capacidad:");
