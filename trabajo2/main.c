42235926968e70473aec98dcfc6722a669789cac
Jesús Toro <jesus.tdd03@gmail.com>
2025-03-24 17:28:53 +0000

    Started minishell implementation

--- trabajo2/main.c
+++ trabajo2/main.c
@@ -3,5 +3,18 @@
 #include "test_sala.h"
 
 int main(int argc, char argv[]) {
+	while (true) {
+		char nombre[10];
+		char ciudad[20];
+		int capacidad;
+		
+		printf("Nombre de la sala:");
+		scanf ("%S", &nombre);
+		
+		printf("Ciudad:");
+		scanf ("%S", &ciudad);
+		printf("Capacidad:");
+		scanf ("%i", &capacidad);
+	}
 }
 
