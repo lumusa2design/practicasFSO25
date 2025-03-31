2851bc8c0d296d147694d3ee82f78b7e1eeaf55c
lumusa2design <lumusa2design@gmail.com>
2025-03-31 17:24:02 +0200

    fixed problems with execlp terminal

--- trabajo2/sucursal.c
+++ trabajo2/sucursal.c
@@ -3,31 +3,31 @@
 #include <string.h>
 #include <unistd.h>
 
-
-
-void crea_sucursal(const char * ciudad,const char * capacidad)
+void crea_sucursal(const char *ciudad, const char *capacidad)
 {
-	pid_t pid;
-	pid = fork();
-	
-	switch (pid)
-	{
-		case -1:
-			perror("fork");
-		case 0:
-			char* args[] = {"xterm","NULL"};
-			printf("Soy el hijo\n");
-			execlp("gnome-terminal","gnome-terminal",NULL);
-		default:
-			printf("Soy el padre\n");
-			
-	}
-	
-	
+    pid_t pid = fork();
+
+    switch (pid)
+    {
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+
+        case 0:
+            printf("Soy el hijo\n");
+            execlp("gnome-terminal", "gnome-terminal", "--", "./minishell", ciudad, capacidad, (char *)NULL);
+            perror("execlp");
+            exit(EXIT_FAILURE);
+
+        default:
+            printf("Soy el padre\n");
+            break;
+    }
 }
 
 int main()
 {
-	crea_sucursal("hola", "5");
-	return 0;
+    crea_sucursal("hola", "5");
+    return 0;
 }
+
