611c38e2cdc313c8ce85f5b44ef62de8c8eff108
Jesús Toro <jesus.tdd03@gmail.com>
2025-05-05 18:14:44 +0100

    Deleted folder recursion.

--- trabajo4/hito3/hito3/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "sala.h"
-#include "test_sala.h"
-
-int main(int argc, char argv[]) {
-	ejecuta_tests();
-	return 0;
-}
-
--- trabajo4/hito3/hito3/test_sala.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_SALA_H_
-#define TEST_SALA_H_
-
-void ejecuta_tests();
-
-#endif /* TEST_SALA_H_ */
