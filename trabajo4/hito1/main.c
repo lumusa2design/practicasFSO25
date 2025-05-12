ff6bd1a2e5146892fa14ca61a3f2e5b9d86af251
Jesús Toro <jesus.tdd03@gmail.com>
2025-05-12 18:10:42 +0100

    Implemented main.

--- trabajo4/hito1/main.c
+++ trabajo4/hito1/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <string.h>
 #include <errno.h>
 #include "sala.h"
@@ -6,11 +8,12 @@
 
 #define ITERACIONES_HILO 3
 #define MAX_PAUSA 1.5
+#define CAPACIDAD_SALA 20
 
 void* hilo_reservas(void* args) {
 	int asientos[ITERACIONES_HILO];
 	for(int i = 0; i < ITERACIONES_HILO; i++) {
-		asiento[i] = reserva_asiento(i+20);
+		asientos[i] = reserva_asiento(i+20);
 		pausa_aleatoria(MAX_PAUSA);
 	}
 	
@@ -21,23 +24,63 @@ void* hilo_reservas(void* args) {
 }
 
 void* hilo_estado(void* args) {
-	estado_sala();
-	pausa_aleatoria(MAX_PAUSA);
+	while(1) {
+		estado_sala();
+		pausa_aleatoria(MAX_PAUSA);
+	}
 }
 
 
-int main(int argc, char argv[]) {
+int ret;
+int main(int argc, char* argv[]) {
 	if (argc != 2) {
-		fprintf(stderr, "Uso: ./ numero_de_hilos");
+		fprintf(stderr, "Uso: ./ numero_de_hilos\n");
+		exit(-1);
 	}
+	
 	
 	int n_threads = atoi(argv[1]);
 	
-	pthread_t threads;
+	pthread_t * threads;
 	if ((threads=malloc(n_threads*sizeof(pthread_t))) == NULL) {
-		fprintf(stderr, "Error al asignar memoria a los hilos: %s", strerror(errno));
+		fprintf(stderr, "Error al asignar memoria a los hilos: %s\n", strerror(errno));
+		exit(-1);
 	}
 	
+	if (crea_sala(CAPACIDAD_SALA) == -1) {
+		fprintf(stderr, "Error al crear la sala.\n");
+		exit(-1);
+	}
 	
+	for (int i = 0; i < n_threads; i++) {
+		if (ret = pthread_create(&threads[i], NULL, hilo_reservas, NULL)) {
+			errno = ret;
+			fprintf(stderr, "Error al crear el hilo %d: %s\n", i, strerror(errno));
+		}
+	}
+	
+	pthread_t state_thread;
+	if (ret = pthread_create(&state_thread, NULL, hilo_estado, NULL)) {
+		errno = ret;
+		fprintf(stderr, "Error al crear el hilo estado: %s\n", strerror(errno));
+		exit(-1);
+	}
+	
+	for (int i = 0; i < n_threads; i++) {
+		if (ret = pthread_join(threads[i], NULL)) {
+			errno = ret;
+			fprintf(stderr, "Error al crear el hilo %d: %s\n", i, strerror(errno));
+			exit(-1);
+		}
+	}
+	
+	pthread_cancel(state_thread);
+	
+	estado_sala();
+	elimina_sala();
+	
+	free(threads);
+	
+	exit(0);
 }
 
